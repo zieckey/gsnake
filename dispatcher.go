2038b9ee4d2b5c0f2fde0764895d34469804d38b
weizili.laptop <zieckey@gmail.com>
2016-01-08 18:53:31 +0800

    Fix dir Suffix slash bug

--- dispatcher.go
+++ dispatcher.go
@@ -33,7 +33,7 @@ func newDispatcher(dir string) (d *Dispatcher, err error) {
 		glog.Fatal(err)
 	}
 
-	d.dir = dir
+	d.dir = strings.TrimSuffix(dir, "/")
 	d.status, err = NewProcessStatus(*statusFile)
 	if err != nil || d.status == nil {
 		glog.Fatal(err)
